ecdadda4fb2e31c830e0ccf1cd16d7697c7be1ce
Yunhui Peng <pengy10@cbbdev21.be-md.ncbi.nlm.nih.gov>
2020-03-25 21:29:44 -0400

    add README to initial commit

---
+++ python2/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /panfs/pan1.be-md.ncbi.nlm.nih.gov/interactomes/env/bin/activate
+python -m analysis
+python -m temp2
---
+++ python3/update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#source /panfs/pan1.be-md.ncbi.nlm.nih.gov/interactomes/env/bin/activate
+export PATH="/home/pengy10/.local/bin:$PATH"
+python -m analysis
+python -m temp2
